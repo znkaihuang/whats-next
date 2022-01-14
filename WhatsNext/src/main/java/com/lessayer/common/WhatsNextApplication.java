7e931e1e93ae7a38ca8ae5fac0a3056f6f554651
znkaihuang <zkhuang@protonmail.com>
2022-01-14 10:04:21 +0800

    Project setup
    Change-Id: Ic48ecef6f6739f1cc3aa50a6b3ebbe2e6b82983f
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

---
+++ WhatsNext/src/main/java/com/lessayer/common/WhatsNextApplication.java
@@ -0,0 +1,13 @@
+package com.lessayer.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsNextApplication.class, args);
+	}
+
+}
---
+++ WhatsNext/src/test/java/com/lessayer/common/WhatsNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessayer.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
