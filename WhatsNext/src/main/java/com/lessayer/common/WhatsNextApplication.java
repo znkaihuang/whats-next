07610e68ba9536ffa4015fc2416f1c0687fe1494
znkaihuang <zkhuang@protonmail.com>
2022-01-14 15:09:42 +0800

    Update the component scan package
    Change-Id: Ie2c1c406f4ea00c4e5c94e86ccfc57e90b205f93
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/common/WhatsNextApplication.java
+++ WhatsNext/src/main/java/com/lessayer/common/WhatsNextApplication.java
@@ -2,8 +2,10 @@ package com.lessayer.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.lessayer.controller")
 public class WhatsNextApplication {
 
 	public static void main(String[] args) {
