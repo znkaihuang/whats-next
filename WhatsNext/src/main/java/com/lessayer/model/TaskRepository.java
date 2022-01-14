f6a17c8652969fb812426b5f85b385dc75ffa799
znkaihuang <zkhuang@protonmail.com>
2022-01-14 16:24:59 +0800

    Add interface TaskRepository
    Change-Id: I84da00c7aaceb83be7dbd6b11c5038c9d2d5000c
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/model/TaskRepository.java
+++ WhatsNext/src/main/java/com/lessayer/model/TaskRepository.java
@@ -4,45 +4,12 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public class TaskRepository implements Iterable<Task> {
-	
-	List<Task> taskList;
-	
-	public TaskRepository() {
-		
-		init();
-		
-	}
-	
-	public void init() {
-		
-		taskList = new ArrayList<Task>();
-		
-	}
-	
-	public boolean addTask(String title, String description, 
-			Date createTime, Date deadline, Priority priority) {
-		
-		taskList.add(new Task(title, description, createTime, deadline, priority));
-		
-		return true;
-		
-	}
-	
-	public List<Task> retrieveAllTasks() {
-		
-		return taskList;
-		
-	}
-
-	@Override
-	public Iterator<Task> iterator() {
-		
-		return taskList.iterator();
-	}
+public interface TaskRepository extends CrudRepository<Task, Long> {
 	
 }
