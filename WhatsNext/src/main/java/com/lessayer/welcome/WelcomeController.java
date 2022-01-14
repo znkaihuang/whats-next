acf4c1ca9ea39a825c1bcc0f2290821b2afb1bad
znkaihuang <zkhuang@protonmail.com>
2022-01-14 11:46:08 +0800

    Add WelcomeController.java
    Change-Id: Iff1f3d97fd036774deb3456d1f0563045796cb68
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

---
+++ WhatsNext/src/main/java/com/lessayer/welcome/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.lessayer.welcome;
+
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping(path = "/welcome")
+	public String showWelcomePage(
+			@RequestParam(name = "name", defaultValue = "User", required = true) String name,
+			Model model) {
+		
+		model.addAttribute("name", name);
+		return "welcome";
+		
+	}
+	
+}
