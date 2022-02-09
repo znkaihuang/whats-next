e917f80d14915f9d61e84ffbd3f3f5c327ab85a0
znkaihuang <zkhuang@protonmail.com>
2022-02-09 17:13:51 +0800

    User service and relative tests.
    Change-Id: Ied706cafa25711e63ec051d149163a78420e3abd
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

---
+++ WhatsNext/src/main/java/com/lessayer/service/UserService.java
@@ -0,0 +1,86 @@
+package com.lessayer.service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lessayer.entity.Role;
+import com.lessayer.entity.User;
+import com.lessayer.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public Optional<User> retrieveUser(String userName) {
+		
+		return repository.findByUserName(userName);
+		
+	}
+	
+	public List<User> retrieveAllAdmins() {
+		
+		return repository.findByRole(Role.ADMIN);
+		
+	}
+	
+	public boolean isUserExist(String userName) {
+		
+		return (retrieveUser(userName).isPresent()) ? true : false;
+		
+	}
+	
+	public void createUser(String userName, String password, String email, Role role)
+		throws RuntimeException {
+		
+		if(isUserExist(userName)) {
+			
+			throw new RuntimeException("The user has already exist!");
+			
+		}
+		else {
+			
+			repository.createUser(new User(userName, password, email, role, 
+					Date.valueOf(LocalDate.now()), Date.valueOf(LocalDate.now())));
+			
+		}
+		
+	}
+	
+	public void deleteUser(String userName) {
+		
+		repository.deleteUser(repository.findByUserName(userName).get().getUserId());
+		
+	}
+	
+	public void updatePassword(String userName, String password) {
+		
+		User user = repository.findByUserName(userName).get();
+		user.setPassword(password);
+		repository.updateUser(user);
+		
+	}
+	
+	public void updateLastLoginDate(String userName, Date date) {
+		
+		User user = repository.findByUserName(userName).get();
+		user.setLastLoginDate(date);
+		repository.updateUser(user);
+		
+	}
+	
+	public void updateEmail(String userName, String email) {
+		
+		User user = repository.findByUserName(userName).get();
+		user.setEmail(email);
+		repository.updateUser(user);
+		
+	}
+	
+}
---
+++ WhatsNext/src/test/java/com/lessayer/service/UserServiceTests.java
@@ -0,0 +1,141 @@
+package com.lessayer.service;
+
+
+import java.sql.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lessayer.entity.Role;
+import com.lessayer.entity.User;
+
+@SpringBootTest
+class UserServiceTests {
+	
+	@Autowired
+	private UserService service;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	void testRetrieveUser() {
+		
+		String userName = "Guest";
+		Optional<User> user = service.retrieveUser(userName);
+		if(user.isEmpty()) {
+			
+			logger.info("No such user {}", userName);
+		
+		}
+		else {
+		
+			logger.info("User info {}", user.get());
+			
+		}
+	}
+	
+	@Test
+	void testRetrieveAllAdmins() {
+		
+		logger.info("List all the administers:\n{}", service.retrieveAllAdmins());
+		
+	}
+	
+	@Test
+	void testIsUserExist() {
+		
+		String userName = "Guest";
+		if(service.isUserExist(userName)) {
+			
+			logger.info("User {} has already existed", userName);
+			
+		}
+		else {	
+			
+			logger.info("User {} does not exist", userName);
+			
+		}
+		
+	}
+
+	@Test
+	void testCreateUser() {
+		
+		String userName = "Terry";
+		String password = "$$12345";
+		String email = "terry@lessayer.com";
+		service.createUser(userName, password, email, Role.USER);
+		logger.info("Create user {}", userName);
+		
+	}
+	
+	@Test
+	void testCreateExistUser() {
+		
+		String userName = "Kevin";
+		String password = "!Ohmygodness8269";
+		String email = "test@test.com";
+		
+		try {
+		
+			service.createUser(userName, password, email, Role.ADMIN);
+		
+		}
+		catch(RuntimeException e) {
+			
+			logger.info("The user {} has already existed.", userName);
+			
+		}
+	}
+	
+	@Test
+	void testDeleteUser() {
+		
+		String userName = "Terry";
+		service.deleteUser(userName);
+		logger.info("Delete user {}", userName);
+		
+	}
+
+	@Test
+	void testUpdatePassword() {
+		
+		String userName = "Mien";
+		String password = "mien@1116";
+		logger.info("Before update password {}", service.retrieveUser(userName)
+				.get().getPassword());
+		service.updatePassword(userName, password);
+		logger.info("After update password {}", service.retrieveUser(userName)
+				.get().getPassword());
+		
+	}
+
+	@Test
+	void testUpdateLastLoginDate() {
+		
+		String userName = "Mien";
+		logger.info("Before update last login date {}", service.retrieveUser(userName)
+				.get().getLastLoginDate());
+		service.updateLastLoginDate(userName, Date.valueOf("2022-04-11"));
+		logger.info("After update last login date {}", service.retrieveUser(userName)
+				.get().getLastLoginDate());
+		
+	}
+	
+	@Test
+	void testUpdateEmail() {
+		
+		String userName = "Guest";
+		logger.info("Before update email {}", service.retrieveUser(userName)
+				.get().getEmail());
+		service.updateEmail(userName, "guest@test.com");
+		logger.info("After update last login date {}", service.retrieveUser(userName)
+				.get().getEmail());
+		
+	}
+	
+}
