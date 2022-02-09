7b8a7eb22bf55c53334bc7f167927c52519e2e3b
znkaihuang <zkhuang@protonmail.com>
2022-02-09 17:13:32 +0800

    User repository and relative tests.
    Change-Id: I68d1e92e0e821b16f127ffd8b57675fc5b672797
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

---
+++ WhatsNext/src/main/java/com/lessayer/repository/UserRepository.java
@@ -0,0 +1,82 @@
+package com.lessayer.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.lessayer.entity.Role;
+import com.lessayer.entity.User;
+
+@Repository
+@Transactional
+public class UserRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	private TypedQuery<User> query;
+	
+	public User findByUserId(Long userId) {
+		
+		return entityManager.find(User.class, userId);
+		
+	}
+	
+	// PSQL Name: "find_user_by_username",
+	// PSQL Query: "select u from User u where u.userName like :username"
+	public Optional<User> findByUserName(String userName) {
+		
+		query = entityManager.createNamedQuery("find_user_by_username", User.class)
+							.setParameter("username", userName);
+		
+		User user = null;
+		try {
+			
+			user = query.getSingleResult();
+		
+		}
+		catch (NoResultException e) {
+			// TODO: handle exception
+		}
+
+		return Optional.ofNullable(user);
+			
+	}
+	
+	// PSQL Name: "find_user_by_role",
+	// PSQL Query: "select u from User u where u.role like :role"
+	public List<User> findByRole(Role role) {
+		
+		query = entityManager.createNamedQuery("find_user_by_role", User.class)
+							.setParameter("role", role);
+
+		return query.getResultList();
+			
+	}
+	
+	public User createUser(User user) {
+	
+		return entityManager.merge(user);
+		
+	}
+	
+	public void deleteUser(Long userId) {
+		
+		entityManager.remove(findByUserId(userId));
+		
+	}
+	
+	public User updateUser(User user) {
+		
+		return entityManager.merge(user);
+		
+	}
+	
+}
---
+++ WhatsNext/src/test/java/com/lessayer/entity/UserRepositoryTests.java
@@ -0,0 +1,78 @@
+package com.lessayer.entity;
+
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lessayer.repository.UserRepository;
+
+@SpringBootTest
+class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Test
+	void testFindByUserId() {
+		
+		Long userId = 1L;
+		logger.info("User info {}", repository.findByUserId(userId));
+	
+	}
+	
+	@Test
+	void testfindByUserName() {
+		
+		String userName = "Kevin";
+		logger.info("User info {}", repository.findByUserName(userName));
+		
+	}
+	
+	@Test
+	void testfindByRole() {
+		
+		Role role = Role.ADMIN;
+		logger.info("User info {}", repository.findByRole(role));
+		
+	}
+	
+	@Test
+	void testCreateUser() {
+		
+		User user = new User("Guest", "guest", "", Role.GUEST, 
+				Date.valueOf(LocalDate.now()), Date.valueOf(LocalDate.now()));		
+		repository.createUser(user);
+		logger.info("Create user {}", user);
+		
+	}
+
+	@Test
+	void testDeleteUser() {
+		
+		String userName = "Guest";
+		User user = repository.findByUserName(userName).get();
+		repository.deleteUser(user.getUserId());
+		logger.info("Delete user {}", user);
+		
+	}
+
+	@Test
+	void testUpdateUser() {
+		
+		String userName = "Guest";
+		User user = repository.findByUserName(userName).get();
+		user.setPassword("guest");
+		repository.updateUser(user);
+		logger.info("Update user {}", user);
+		
+	}
+
+}
