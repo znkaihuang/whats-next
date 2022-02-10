ee0bd486fd19fc1e915ecf34456f298384942467
znkaihuang <zkhuang@protonmail.com>
2022-02-10 20:57:12 +0800

    Use customUserDetailsManager in ServiceConfig
    Change-Id: Ica4d2454b15fc97b763980286012b587745a1942
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/securing/SecurityConfig.java
+++ WhatsNext/src/main/java/com/lessayer/securing/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.lessayer.securing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,12 +11,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.lessayer.service.CustomUserDetailsManager;
+import com.lessayer.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private UserService service;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
@@ -31,20 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user1 =
-			 User.withDefaultPasswordEncoder()
-				.username("kevin")
-				.password("ohmygod123")
-				.roles("ADMIN")
-				.build();
-		UserDetails user2 =
-				 User.withDefaultPasswordEncoder()
-					.username("mien")
-					.password("test")
-					.roles("USER")
-					.build();
-			
-		return new InMemoryUserDetailsManager(user1, user2);
+		
+		return new CustomUserDetailsManager(service);
+		
 	}
 	
 }
