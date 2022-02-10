4fcd90f8b6e75e5d198d90179e8254f8a100f2a3
znkaihuang <zkhuang@protonmail.com>
2022-02-10 20:56:00 +0800

    Add session attribute userName
    Change-Id: I42847eb23a1721c4d1ac01d24103ad96a730937a
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
+++ WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
@@ -1,15 +1,23 @@
 package com.lessayer.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("userName")
 public class WelcomeController {
 	
 	@GetMapping("/")
-	public String showWelcomePage() {
+	public String showWelcomePage(ModelMap model) {
+		
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		model.put("userName", authentication.getName());
 		
 		return "welcome";
 		
