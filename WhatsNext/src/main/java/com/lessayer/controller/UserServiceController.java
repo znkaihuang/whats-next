c79d32bda375bac4df77750778c030520c703cda
znkaihuang <zkhuang@protonmail.com>
2022-02-14 16:14:15 +0800

    Add UserServiceController and userlist.html
    Change-Id: I96c42414a5cee1288cee1191819ca4f3d9994c30
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/controller/UserServiceController.java
+++ WhatsNext/src/main/java/com/lessayer/controller/UserServiceController.java
@@ -1,5 +1,43 @@
 package com.lessayer.controller;
 
-public class UserServiceController {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.lessayer.entity.User;
+import com.lessayer.service.UserService;
+
+@Controller
+@SessionAttributes({"userName", "isAdmin"})
+public class UserServiceController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/user-list")
+	public String showUserList() {
+		
+		return "userlist";
+		
+	}
+	
+	
+	@ModelAttribute("users")
+	public List<User> populateAllUsers() {
+		
+		List<User> admins = userService.retrieveAllAdmins();
+		List<User> users = userService.retrieveAllUsers();
+		List<User> allUsers = Stream.concat(admins.stream(), users.stream())
+									.toList();
+		return allUsers;
+		
+	}
+	
 }
