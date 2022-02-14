93ebc8312b02234252779303e6b89955fc237fca
znkaihuang <zkhuang@protonmail.com>
2022-02-14 16:10:44 +0800

    Add session attribute "isAdmin" in WelcomeController
    Change-Id: Iab8479e1a1eb3369071ef4a5a60a4483fc88943d
    Signed-off-by: znkaihuang <zkhuang@protonmail.com>

--- WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
+++ WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.lessayer.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,16 +10,26 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.lessayer.entity.Role;
+import com.lessayer.service.UserService;
+
 @Controller
-@SessionAttributes("userName")
+@SessionAttributes({"userName", "isAdmin"})
 public class WelcomeController {
+	
+	@Autowired
+	UserService userService;
 	
 	@GetMapping("/")
 	public String showWelcomePage(ModelMap model) {
 		
 		SecurityContext context = SecurityContextHolder.getContext();
 		Authentication authentication = context.getAuthentication();
-		model.put("userName", authentication.getName());
+		String userName = authentication.getName();
+		Boolean isAdmin = (userService.retrieveUser(userName).get().getRole() == Role.ADMIN)
+							? true : false;
+		model.put("userName", userName);
+		model.put("isAdmin", isAdmin);
 		
 		return "welcome";
 		
