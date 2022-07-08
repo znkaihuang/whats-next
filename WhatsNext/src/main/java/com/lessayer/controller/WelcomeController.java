a6c561d5079e26ae85200e0337120e22a38651e6
znkaihuang <zkhuang@protonmail.com>
2022-07-08 15:59:21 +0800

    Refacotor home page code with the new introduced TaskDivSetting class

--- WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
+++ WhatsNext/src/main/java/com/lessayer/controller/WelcomeController.java
@@ -1,6 +1,9 @@
 package com.lessayer.controller;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -8,8 +11,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.lessayer.TaskDivSetting;
 import com.lessayer.entity.Role;
 import com.lessayer.service.UserService;
 
@@ -18,7 +23,9 @@ import com.lessayer.service.UserService;
 public class WelcomeController {
 	
 	@Autowired
-	UserService userService;
+	private UserService userService;
+	
+	private List<TaskDivSetting> taskDivs = new ArrayList<>();
 	
 	@GetMapping("/")
 	public String showWelcomePage(ModelMap model) {
@@ -31,9 +38,23 @@ public class WelcomeController {
 		model.put("userName", userName);
 		model.put("isAdmin", isAdmin);
 		model.put("currentPage", "home");
+		model.put("taskDivStyles", isAdmin);
 		
 		return "welcome";
 		
+	}
+	
+	@ModelAttribute("taskDivs")
+	public List<TaskDivSetting> populateTaskDivs() {
+		
+		if (taskDivs.isEmpty()) {
+			taskDivs.add(new TaskDivSetting("Important Urgent Tasks", "first-task-div"));
+			taskDivs.add(new TaskDivSetting("Important Not Urgent Tasks", "second-task-div"));
+			taskDivs.add(new TaskDivSetting("Not Important Urgent Tasks", "third-task-div"));
+			taskDivs.add(new TaskDivSetting("Not Important Not Urgent Tasks", "forth-task-div"));
+		}
+		
+		return taskDivs;
 	}
 	
 }
