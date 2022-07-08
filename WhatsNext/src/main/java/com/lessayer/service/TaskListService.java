20e5d9a7ddcafb64a5f69d8fff1ebf984ed4d761
znkaihuang <zkhuang@protonmail.com>
2022-07-08 12:13:05 +0800

    Formate the code of TaskListService

--- WhatsNext/src/main/java/com/lessayer/service/TaskListService.java
+++ WhatsNext/src/main/java/com/lessayer/service/TaskListService.java
@@ -15,136 +15,130 @@ import com.lessayer.repository.TaskRepository;
 
 @Service
 public class TaskListService {
-	
+
 	@Autowired
 	private TaskRepository repository;
-	
+
 	public final Integer TASKS_PER_PAGE = 8;
 	private Long userId;
 	private List<Task> taskListCache;
 	private Boolean updateCache = false;
 	private Integer totalPage;
 	private Integer totalTaskNum;
-	
+
 	public void setUserId(Long userId) {
-		
+
 		this.userId = userId;
-		
+
 	}
-	
+
 	public Optional<List<Task>> retrieveTasks() {
-		
+
 		Optional<List<Task>> taskListCacheOptional = Optional.ofNullable(taskListCache);
 		if (taskListCacheOptional.isEmpty() || updateCache) {
 			taskListCache = repository.findByUserId(this.userId);
 			totalTaskNum = taskListCache.size();
 			if (totalTaskNum == 0) {
 				totalPage = 0;
-			}
-			else if (totalTaskNum % TASKS_PER_PAGE == 0) {
+			} else if (totalTaskNum % TASKS_PER_PAGE == 0) {
 				totalPage = (taskListCache.size() / TASKS_PER_PAGE);
-			}
-			else {
+			} else {
 				totalPage = (taskListCache.size() / TASKS_PER_PAGE) + 1;
 			}
 			updateCache = false;
-			
+
 			return Optional.ofNullable(taskListCache);
-		}
-		else {
+		} else {
 			return taskListCacheOptional;
 		}
 	}
-	
+
 	public Optional<List<Task>> retrieveTasksByPage(Integer pageNum) {
-		
+
 		Optional<List<Task>> taskListOptional = retrieveTasks();
 		if (taskListOptional.isEmpty()) {
 			return taskListOptional;
-		}
-		else {
+		} else {
 			List<Task> taskList = taskListOptional.get();
 			sortTasksById(taskList, updateCache);
-			
-			return Optional.ofNullable(taskList.subList((pageNum - 1) * TASKS_PER_PAGE, 
+
+			return Optional.ofNullable(taskList.subList((pageNum - 1) * TASKS_PER_PAGE,
 					Math.min(taskListOptional.get().size(), pageNum * TASKS_PER_PAGE)));
 		}
-		
+
 	}
-	
+
 	public Optional<List<Task>> retrieveTasksByPage(Integer pageNum, String sortField, Boolean ascending) {
-		
+
 		Optional<List<Task>> taskListOptional = retrieveTasks();
 		if (taskListOptional.isEmpty()) {
 			return taskListOptional;
-		}
-		else {
+		} else {
 			List<Task> taskList = taskListOptional.get();
-			
+
 			switch (sortField) {
-				case "title":
-					sortTasksByTitle(taskList, ascending);
-					break;
-				case "startDate":
-					sortTasksByStartDate(taskList, ascending);
-					break;
-				case "endDate":
-					sortTasksByEndDate(taskList, ascending);
-					break;
-				case "priority":
-					sortTasksByPriority(taskList, ascending);
-					break;
-				case "status":
-					sortTasksByStatus(taskList, ascending);
-					break;
-				default:
-					break;
+			case "title":
+				sortTasksByTitle(taskList, ascending);
+				break;
+			case "startDate":
+				sortTasksByStartDate(taskList, ascending);
+				break;
+			case "endDate":
+				sortTasksByEndDate(taskList, ascending);
+				break;
+			case "priority":
+				sortTasksByPriority(taskList, ascending);
+				break;
+			case "status":
+				sortTasksByStatus(taskList, ascending);
+				break;
+			default:
+				break;
 			}
-			
-			return Optional.ofNullable(taskList.subList((pageNum - 1) * TASKS_PER_PAGE, 
+
+			return Optional.ofNullable(taskList.subList((pageNum - 1) * TASKS_PER_PAGE,
 					Math.min(taskListOptional.get().size(), pageNum * TASKS_PER_PAGE)));
 		}
-		
+
 	}
-	
+
 	public Integer getTotalPage() {
 		return totalPage;
 	}
-	
+
 	public Integer getTotalTaskNum() {
 		return totalTaskNum;
 	}
-	
+
 	// To prevent accessing other user's tasks, would check the user id first.
 	public Optional<Task> retrieveTaskById(Long taskId) {
-		
+
 		Task task = repository.findByTaskId(taskId);
-		if(task.getUserId() == this.userId) {
-			
+		if (task.getUserId() == this.userId) {
+
 			return Optional.ofNullable(task);
-			
-		}
-		else {
-			
+
+		} else {
+
 			return Optional.empty();
-			
+
 		}
-		
+
 	}
 
-	public Long createTask(String title, String description, 
-			Date startDate, Date endDate, Priority priority, TaskStatus status) {
-		
+	public Long createTask(String title, String description, Date startDate, Date endDate, Priority priority,
+			TaskStatus status) {
+
 		Task task = new Task(userId, title, description, startDate, endDate, priority, status);
 		updateCache = true;
-		
+
 		return repository.createTask(task).getTaskId();
-		
+
 	}
 
-	public void updateTask(long taskId, String title, String description, 
-			Date startDate, Date endDate, Priority priority, TaskStatus status) {
-		
+	public void updateTask(long taskId, String title, String description, Date startDate, Date endDate,
+			Priority priority, TaskStatus status) {
+
 		Task task = repository.findByTaskId(taskId);
 		task.setTitle(title);
 		task.setDescription(description);
@@ -154,67 +148,66 @@ public class TaskListService {
 		task.setStatus(status);
 		repository.updateTask(task);
 		updateCache = true;
-		
+
 	}
 
 	public void deleteTask(Long taskId) {
-		
+
 		repository.deleteTask(taskId);
 		updateCache = true;
-		
+
 	}
-	
+
 	public void sortTasksById(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			Long.compare(task1.getTaskId(), task2.getTaskId()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> Long.compare(task1.getTaskId(), task2.getTaskId())
+				* factor;
 		taskList.sort(comparator);
-		
+
 	}
 
 	public void sortTasksByTitle(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			task1.getTitle().compareTo(task2.getTitle()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> task1.getTitle().compareTo(task2.getTitle()) * factor;
 		taskList.sort(comparator);
-		
+
 	}
 
 	public void sortTasksByStartDate(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			task1.getStartDate().compareTo(task2.getStartDate()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> task1.getStartDate().compareTo(task2.getStartDate())
+				* factor;
 		taskList.sort(comparator);
-		
+
 	}
-	
+
 	public void sortTasksByEndDate(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			task1.getEndDate().compareTo(task2.getEndDate()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> task1.getEndDate().compareTo(task2.getEndDate())
+				* factor;
 		taskList.sort(comparator);
-		
+
 	}
-	
+
 	public void sortTasksByPriority(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			task1.getPriority().compareTo(task2.getPriority()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> task1.getPriority().compareTo(task2.getPriority())
+				* factor;
 		taskList.sort(comparator);
-		
+
 	}
-	
+
 	public void sortTasksByStatus(List<Task> taskList, Boolean ascendingOrder) {
-		
+
 		Integer factor = (ascendingOrder) ? 1 : -1;
-		Comparator<Task> comparator = (Task task1, Task task2) -> 
-			task1.getStatus().compareTo(task2.getStatus()) * factor;
+		Comparator<Task> comparator = (Task task1, Task task2) -> task1.getStatus().compareTo(task2.getStatus())
+				* factor;
 		taskList.sort(comparator);
-		
+
 	}
 }
