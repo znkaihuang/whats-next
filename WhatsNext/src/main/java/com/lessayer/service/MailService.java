153c7762f9d31f718a74c875c3f66ead069a3a3a
znkaihuang <zkhuang@protonmail.com>
2022-07-11 22:00:16 +0800

    Refactor MailService and MailServiceConfiguration

--- WhatsNext/src/main/java/com/lessayer/service/MailService.java
+++ WhatsNext/src/main/java/com/lessayer/service/MailService.java
@@ -1,33 +1,34 @@
 package com.lessayer.service;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MailService {
-	
+
 	private MailServiceConfiguration configuration;
-	
-    private JavaMailSender emailSender;
-    
-    public void config(MailServiceConfiguration configuration) {
-    	
-    	this.configuration = configuration;
-    	this.emailSender = configuration.getMailSender();
-    	
+
+	private JavaMailSender emailSender;
+
+    @Bean
+    void configMail() {
+
+        this.configuration = new MailServiceConfiguration("host", 123, "userName", "password");
+        this.emailSender = configuration.getMailSender();
+
     }
-    
-    public void sendSimpleMessage(String to, String subject, String text) {
 
-    	        SimpleMailMessage message = new SimpleMailMessage(); 
-    	        message.setFrom(this.configuration.getUserName());
-    	        message.setTo(to); 
-    	        message.setSubject(subject); 
-    	        message.setText(text);
-    	        emailSender.send(message);
+	public void sendSimpleMessage(String to, String subject, String text) {
 
-   }
-    
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(this.configuration.getUserName());
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(text);
+		emailSender.send(message);
+
+	}
+
 }
--- WhatsNext/src/main/java/com/lessayer/service/MailServiceConfiguration.java
+++ WhatsNext/src/main/java/com/lessayer/service/MailServiceConfiguration.java
@@ -8,69 +8,82 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MailServiceConfiguration {
-	
+
 	private String host;
 	private Integer port;
 	private String userName;
 	private String password;
 	
-	public String getHost() {
+	public MailServiceConfiguration() {
+		
+	}
 	
+	public MailServiceConfiguration(String host, Integer Port, String userName, String password) {
+		
+		this.host = host;
+		this.port = Port;
+		this.userName = userName;
+		this.password = password;
+		
+	}
+
+	public String getHost() {
+
 		return host;
 	}
 
 	public void setHost(String host) {
-	
+
 		this.host = host;
 	}
 
 	public Integer getPort() {
-	
+
 		return port;
 	}
 
 	public void setPort(Integer port) {
-	
+
 		this.port = port;
 	}
 
 	public String getUserName() {
-	
+
 		return userName;
 	}
 
 	public void setUserName(String userName) {
-	
+
 		this.userName = userName;
 	}
 
 	public String getPassword() {
-	
+
 		return password;
 	}
 
 	public void setPassword(String password) {
-	
+
 		this.password = password;
 	}
 
 	public JavaMailSender getMailSender() {
-		
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost(host);
-	    mailSender.setPort(port);
-	    
-	    mailSender.setUsername(userName);
-	    mailSender.setPassword(password);
-	    
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	    
-	    return mailSender;
-		
+
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+
+		mailSender.setUsername(userName);
+		mailSender.setPassword(password);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+
 	}
-	
+
 }
